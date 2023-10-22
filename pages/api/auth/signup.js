88276e91a2823068ffc795b1cb0e599cd823bdec
BillStephens2022 <two4onebill@yahoo.com>
2023-10-22 16:31:13 -0400

    udpated back end code for signing up or logging in

---
+++ pages/api/auth/signup.js
@@ -0,0 +1,40 @@
+import { hashPassword } from "../../../lib/auth";
+import connectToDatabase from "../../../lib/db";
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+    const { username, password } = data;
+    if (
+      !username ||
+      !password ||
+      password.trim().length < 7
+    ) {
+      res.status(422).json({
+        message:
+          "Invalid input - password should be at least 7 characters long.",
+      });
+      return;
+    }
+    const client = await connectToDatabase();
+    const db = client.db();
+
+    const hashedPassword = await hashPassword(password);
+
+    const existingUser = await db.collection("users").findOne({username: username});
+    if (existingUser) {
+        res.status(422).json({ message: "User exists already!"});
+        client.close();
+        return;
+    }
+    const result = await db.collection("users").insertOne({
+      username: username,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({ message: "Created User!" });
+    client.close();
+  }
+}
+
+export default handler;
