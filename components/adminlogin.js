2c9392d9b0adc78dac635c70cd581821b6fa17bc
BillStephens2022 <two4onebill@yahoo.com>
2023-10-22 16:09:22 -0400

    used useRef hook to capture login and signup form inputs

--- components/adminlogin.js
+++ components/adminlogin.js
@@ -1,13 +1,24 @@
-import { Fragment, useState } from "react";
+import { Fragment, useState, useRef } from "react";
 import classes from "./adminlogin.module.css";
 
 function AdminLogin() {
+  const usernameInputRef = useRef();
+  const passwordInputRef = useRef();
+
+  const enteredUsername = usernameInputRef.current.value;
+  const enteredPassword = passwordInputRef.current.value;
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    console.log("attempting to log in!", enteredUsername, enteredPassword);
+  }
+
   return (
     <Fragment>
      <h2 className={classes.form_header}>Admin Signup</h2>
       <div className={classes.form_container}>
        
-        <form className={classes.form}>
+        <form className={classes.form} onSubmit={handleSubmit}>
           <div>
             <label htmlFor="username" className={classes.label}>Username</label>
             <input
@@ -16,6 +27,7 @@ function AdminLogin() {
               placeholder="username"
               className={classes.input}
               id="username"
+              ref={usernameInputRef}
             />
           </div>
           <div>
@@ -26,9 +38,12 @@ function AdminLogin() {
               placeholder="password"
               className={classes.input}
               id="password"
+              ref={passwordInputRef}
             />
           </div>
+          <button type="button" onClick={handleSubmit}>Submit</button>
         </form>
+
       </div>
     </Fragment>
   );
--- components/adminsignup.js
+++ components/adminsignup.js
@@ -1,12 +1,25 @@
-import { Fragment, useState } from "react";
+import { Fragment, useState, useRef } from "react";
 import classes from "./adminlogin.module.css";
 
 function AdminSignup() {
+  const usernameInputRef = useRef();
+  const passwordInputRef = useRef();
+  const confirmPasswordInputRef = useRef();
+
+  const enteredUsername = usernameInputRef.current.value;
+  const enteredPassword = passwordInputRef.current.value;
+  const enteredConfirmedPassword = confirmPasswordInputRef.current.value;
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    console.log("attempting to signup!", enteredUsername, enteredPassword, enteredConfirmedPassword);
+  }
+
   return (
     <Fragment>
       <h2 className={classes.form_header}>Admin Login</h2>
       <div className={classes.form_container}>
-        <form className={classes.form}>
+        <form className={classes.form} onSubmit={handleSubmit}>
           <div>
             <label htmlFor="username" className={classes.label}>Username</label>
             <input
@@ -15,6 +28,7 @@ function AdminSignup() {
               placeholder="username"
               className={classes.input}
               id="username"
+              ref={usernameInputRef}
             />
           </div>
           <div>
@@ -25,6 +39,7 @@ function AdminSignup() {
               placeholder="password"
               className={classes.input}
               id="password"
+              ref={passwordInputRef}
             />
           </div>
           <div>
@@ -35,8 +50,10 @@ function AdminSignup() {
               placeholder="confirm password"
               className={classes.input}
               id="confirm-password"
+              ref={confirmPasswordInputRef}
             />
           </div>
+          <button type="button" onClick={handleSubmit}>Submit</button>
         </form>
       </div>
     </Fragment>
