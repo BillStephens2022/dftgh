93f2ad8a26723fbb0a4844ff1a110d1f216622d9
BillStephens2022 <two4onebill@yahoo.com>
2023-10-24 13:24:10 -0400

    embedded Apple Podcast player in Home Page and Listen Page

--- pages/index.js
+++ pages/index.js
@@ -12,8 +12,36 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={classes.main}>
-        <h1 className={classes.title}>Drinking from the Garden Hose</h1>
-        <Image src="/DFTGH.webp" alt="Podcast Logo" width={600} height={600}></Image>
+        <div className={classes.container_grid}>
+          <h1 className={classes.title}>Drinking from the Garden Hose</h1>
+          <div className={classes.grid_1}>
+            <Image
+              src="/DFTGH.webp"
+              alt="Podcast Logo"
+              width={600}
+              height={600}
+            ></Image>
+          </div>
+          <div className={classes.grid_2}>
+            <iframe
+              id="embedPlayer"
+              src="https://embed.podcasts.apple.com/us/podcast/drinking-from-the-garden-hose/id1572514520?itsct=podcast_box_player&amp;itscg=30200&amp;ls=1&amp;theme=auto"
+              height="450px"
+              frameBorder="0"
+              sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-top-navigation-by-user-activation"
+              allow="autoplay *; encrypted-media *; clipboard-write"
+              style={{
+                width: "100%",
+                maxWidth: "660px",
+                overflow: "hidden",
+                borderRadius: "10px",
+                transform: "translateZ(0px)",
+                animation: "2s ease 0s 6 normal none running loading-indicator",
+                backgroundColor: "rgb(228, 228, 228)",
+              }}
+            ></iframe>
+          </div>
+        </div>
       </main>
     </>
   );
