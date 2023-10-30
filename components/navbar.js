00b8dfad05eabf775530b3f08e981523e9e86eb3
BillStephens2022 <two4onebill@yahoo.com>
2023-10-30 15:35:14 -0400

    updated navbar to underline active nav item

--- components/navbar.js
+++ components/navbar.js
@@ -1,6 +1,7 @@
 import { useSession } from "next-auth/react";
 import { Fragment } from "react";
 import { signOut } from "next-auth/react";
+import { useRouter } from "next/router";
 import Image from "next/image";
 import Link from "next/link";
 import Button from "./button";
@@ -8,6 +9,7 @@ import classes from "./navbar.module.css";
 
 function Navbar() {
   const { data: session } = useSession();
+  const router = useRouter();
 
   function logoutHandler() {
     signOut();
@@ -28,23 +30,23 @@ function Navbar() {
                 alt="logo"
                 height={60}
                 width={60}
-                style={imageStyle}
+                style={imageStyle}f
               />
             </Link>
           </div>
           
           <ul className={classes.nav_items}>
             <li className={classes.nav_item}>
-              <Link href="/bios" className={classes.nav_item}>Bios</Link>
+              <Link href="/bios" className={`${classes.nav_item} ${classes.link} ${router.pathname === "/bios" ? classes.active : ""}`}>Bios</Link>
             </li>
             <li className={classes.nav_item}>
-              <Link href="/episodes" className={classes.nav_item}>Episodes</Link>
+              <Link href="/episodes" className={`${classes.nav_item} ${classes.link} ${router.pathname === "/episodes" ? classes.active : ""}`}>Episodes</Link>
             </li>
             <li className={classes.nav_item}>
-              <Link href="/testimonials" className={classes.nav_item}>Testimonials</Link>
+              <Link href="/testimonials" className={`${classes.nav_item} ${classes.link} ${router.pathname === "/testimonials" ? classes.active : ""}`}>Testimonials</Link>
             </li>
             <li className={classes.nav_item}>
-              <Link href="/feedback" className={classes.nav_item}>Feedback</Link>
+              <Link href="/feedback" className={`${classes.nav_item} ${classes.link} ${router.pathname === "/feedback" ? classes.active : ""}`}>Feedback</Link>
             </li>
             {session && (
             <div className={classes.nav_items}>
