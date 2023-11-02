306349420ab99c777ee06170e98ef2c6a2c0da58
BillStephens2022 <two4onebill@yahoo.com>
2023-11-02 15:21:14 -0400

    added back end api route for handling password changes

---
+++ pages/api/auth/change-password.js
@@ -0,0 +1,39 @@
+// api/auth/change-password.js
+import dbConnect from "@/components/lib/db";
+import { getSession } from "next-auth/react";
+import User from "@/models/User";
+import { hashPassword, verifyPassword } from "@/components/lib/auth";
+
+export default async function handler(req, res) {
+  const session = await getSession({ req });
+
+  if (!session) {
+    return res.status(401).json({ message: "Not authenticated" });
+  }
+
+  if (req.method !== "POST") {
+    return res.status(405).json({ message: "Method Not Allowed" });
+  }
+
+  await dbConnect();
+
+  const { oldPassword, newPassword } = req.body;
+  const user = await User.findOne({ username: session.user.username });
+
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  const isValid = await verifyPassword(oldPassword, user.password);
+
+  if (!isValid) {
+    return res.status(401).json({ message: "Invalid old password" });
+  }
+
+  const hashedNewPassword = await hashPassword(newPassword);
+
+  user.password = hashedNewPassword;
+  await user.save();
+
+  return res.status(200).json({ message: "Password changed successfully" });
+}
