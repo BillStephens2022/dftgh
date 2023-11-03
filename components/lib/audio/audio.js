4457d7c81b97b08000d1ff1fa3c3b2906787c49b
BillStephens2022 <two4onebill@yahoo.com>
2023-11-03 09:15:36 -0400

    created audioFiles object to be imported into the Bios page

---
+++ components/lib/audio/audio.js
@@ -0,0 +1,38 @@
+const edUrinalSoundClip = new Audio("/audio/ed-urinal.m4a");
+const edGirldadSoundClip = new Audio("/audio/ed-girldad.m4a");
+const edSnakeSoundClip = new Audio("/audio/ed-snake.m4a");
+const edSnoreSoundClip = new Audio("/audio/ed-snore.m4a");
+const edTravelSoundClip = new Audio("/audio/ed-travel.m4a");
+const edKingSoundClip = new Audio("/audio/ed-king.m4a");
+const edGiselleSoundClip = new Audio("/audio/ed-giselle.m4a");
+const edSantaSoundClip = new Audio("/audio/ed-santa.m4a");
+const edToesSoundClip = new Audio("/audio/ed-toes.m4a");
+const obBoyDadSoundClip = new Audio("/audio/OB-boydad.m4a");
+const obTesticsSoundClip = new Audio("/audio/OB-testics.m4a");
+const obSwordsSoundClip = new Audio("/audio/OB-swords.m4a");
+const obVacationSoundClip = new Audio("/audio/OB-vacation.m4a");
+const obDWMSoundClip = new Audio("/audio/OB-DWM.m4a");
+const obRawdogSoundClip = new Audio("/audio/OB-rawdog.m4a")
+const obBearSoundClip = new Audio("/audio/OB-bear.m4a");
+const obSockieSoundClip = new Audio("/audio/OB-sockie.m4a");
+const ericSoundClip = new Audio("sounds/eric.m4a");
+
+
+const audioFiles = {
+    "ed-girldad": edGirldadSoundClip,
+    "ed-snake": edSnakeSoundClip,
+    "ed-snore": edSnoreSoundClip,
+    "ed-travel": edTravelSoundClip,
+    "ed-king": edKingSoundClip,
+    "ed-giselle": edGiselleSoundClip,
+    "ed-santa": edSantaSoundClip,
+    "ob-boydad": obBoyDadSoundClip,
+    "ob-swords": obSwordsSoundClip,
+    "ob-vacation": obVacationSoundClip,
+    "ob-dwm": obDWMSoundClip,
+    "ob-rawdog": obRawdogSoundClip,
+    "ob-bear": obBearSoundClip,
+    "ob-sockie": obSockieSoundClip
+};
+
+export default audioFiles;
--- pages/bios/index.js
+++ pages/bios/index.js
@@ -1,5 +1,6 @@
 import { Fragment } from "react";
 import Image from "next/image";
+import audioFiles from "@/components/lib/audio/audio";
 import classes from "./bios.module.css";
 
 function Bios() {
@@ -13,8 +14,8 @@ function Bios() {
       <main className={classes.main}>
         <h1 className={classes.title}>Bios</h1>
         <div className={classes.container}>
-          <div>
-            <h2 className={classes.subtitle}>Meet Ed</h2>
+          <div className={classes.ed_div}>
+            <h2 className={classes.subtitle}>Meet Ed ("Roadkill")</h2>
             <Image
               src="/images/ed.jpeg"
               width={300}
@@ -22,9 +23,22 @@ function Bios() {
               alt="Photo of Ed"
               style={imageStyle}
             />
+            <p id="ed-girldad">
+              <span id="pink">girl</span> dad
+            </p>
+            <p id="ed-snake">crippling fear of small woodpile snakes</p>
+            <p id="ed-snore">major snoring problem</p>
+            <p id="ed-travel">gets anxious before traveling</p>
+            <p id="ed-king">
+              thinks if you marry a queen you should be a "king"
+            </p>
+            <p id="ed-giselle">
+              has his own take on Brazilian naming conventions
+            </p>
+            <p id="ed-santa">thinks Santa Claus is biblical</p>
           </div>
-          <div>
-            <h2 className={classes.subtitle}>Meet Ob</h2>
+          <div className={classes.ob_div}>
+            <h2 className={classes.subtitle}>Meet OB ("Flounder")</h2>
             <Image
               src="/images/ob.jpeg"
               width={300}
@@ -32,6 +46,19 @@ function Bios() {
               alt="Photo of OB"
               style={imageStyle}
             />
+            <p id="ob-boydad">
+              <span id="blue">boy</span> dad
+            </p>
+            <p id="ob-swords">swordfighter (he calls it "martial arts")</p>
+            <p id="ob-vacation">
+              will only travel with 3 out of 4 family members
+            </p>
+            <p id="ob-dwm">once mansplained "dancing with myself"</p>
+            <p id="ob-rawdog">used the term "rawdogging" on air</p>
+            <p id="ob-bear">thinks he can take on any bear with a machete</p>
+            <p id="ob-sockie">
+              wears "sockie" type shoes when playing with swords
+            </p>
           </div>
         </div>
       </main>
