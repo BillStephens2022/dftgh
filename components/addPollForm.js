f4c686e5b8307d53f17365ee444bd7305b02fa36
BillStephens2022 <two4onebill@yahoo.com>
2023-11-05 08:42:32 -0500

    created new addPollForm component

---
+++ components/addPollForm.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+const PollForm = () => {
+  const [question, setQuestion] = useState('');
+  const [options, setOptions] = useState(['', '', '', '']);
+
+  const handleOptionChange = (index, value) => {
+    const newOptions = [...options];
+    newOptions[index] = value;
+    setOptions(newOptions);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log("adding a new poll!");
+  };
+
+  return (
+    <form onSubmit={handleFormSubmit}>
+      <label>
+        Question:
+        <input type="text" value={question} onChange={(e) => setQuestion(e.target.value)} required />
+      </label>
+      <br />
+      {options.map((option, index) => (
+        <div key={index}>
+          <label>
+            Option {index + 1}:
+            <input type="text" value={option} onChange={(e) => handleOptionChange(index, e.target.value)} required={index < 2} />
+          </label>
+          <br />
+        </div>
+      ))}
+      <button type="submit">Create Poll</button>
+    </form>
+  );
+};
+
+export default PollForm;
