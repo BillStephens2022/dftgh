5a31ebe2246d7b0df49660f451a9d44353ee11c4
BillStephens2022 <two4onebill@yahoo.com>
2023-11-07 09:29:56 -0500

    updated styling

--- pages/episodes/[episodeId].js
+++ pages/episodes/[episodeId].js
@@ -36,7 +36,6 @@ function calculatePercentage(votes, totalVotes) {
   return (votes / totalVotes) * 100;
 }
 
-
 function EpisodeDetail() {
   const router = useRouter();
   const { data: session } = useSession();
@@ -223,9 +222,7 @@ function EpisodeDetail() {
         // Update the episode state to remove the deleted poll
         setEpisode((prevEpisode) => ({
           ...prevEpisode,
-          polls: prevEpisode.polls.filter(
-            (poll) => poll._id !== pollId
-          ),
+          polls: prevEpisode.polls.filter((poll) => poll._id !== pollId),
         }));
       } else {
         console.error("Error deleting poll");
@@ -233,158 +230,187 @@ function EpisodeDetail() {
     } catch (error) {
       console.error(error.message);
     }
-  }
+  };
 
   // Define an array of poll results rendering
   const pollResultBarColors = ["red", "lightblue", "lightyellow", "lightgreen"];
 
   return (
     <div className={classes.episodeId_div}>
-      <div className={classes.addPollButton_div}>
-        <Button
-          text="Add Poll"
-          backgroundColor="steelblue"
-          color="white"
-          onClick={openModal}
-        />
+      <div className={classes.header_card}>
+        <div className={classes.image_div}>
+          <img src={episode.imageLink} width={200} height={200}></img>
+        </div>
+        <div className={classes.header_card_content}>
+          <h1 className={classes.title}>{episode.title}</h1>
+          <p className={classes.date_aired}>
+            Aired: {formatDate(episode.dateAired)}
+          </p>
+          <p className={classes.description}>{episode.description}</p>
+        </div>
       </div>
-      {modalOpen && (
-        <ModalForm
-          onClose={closeModal}
-          modalTitle={"Add Poll"}
-          modalOpen={modalOpen}
-          setModalOpen={setModalOpen}
-          form={
-            <AddPollForm
-              onSubmit={handleAddPoll}
-              onSubmitSuccess={closeModal}
+      <div className={classes.polls_and_comments_div}>
+        <div className={classes.polls_div}>
+          <h3 className={classes.subheading_h3}>Polls</h3>
+          <div className={classes.addPollButton_div}>
+            <Button
+              text="Add Poll"
+              backgroundColor="steelblue"
+              color="white"
+              onClick={openModal}
             />
-          }
-        />
-      )}
-      <h1 className={classes.title}>{episode.title}</h1>
-
-      <p className={classes.description}>{episode.description}</p>
-      <div className={classes.image_div}>
-        <img src={episode.imageLink} width={200} height={200}></img>
-      </div>
-      <p className={classes.date_aired}>{formatDate(episode.dateAired)}</p>
-
-      <h3 className={classes.subheading_h3}>Polls</h3>
-      {episode.polls.map((poll) => {
-        const pollHasVoted = hasVoted[poll._id];
-        const totalVotes = poll.options.reduce((acc, option) => acc + option.votes, 0);
-        return (
-          <div className={classes.poll_div} key={poll._id}>
-            <button type="button" className={classes.delete_btn} onClick={() => handleDeletePoll(episodeId, poll._id)}>x</button>
-            <p className={classes.poll_question}>{poll.question}</p>
-            {pollHasVoted ? (
-              // Render results if the user has voted
-              <ul className={classes.poll_ul}>
-                {poll.options.map((option, index) => (
-                  <li className={classes.poll_li_results} key={option._id}>
-                  {option.text}: {option.votes} votes ({Math.round(calculatePercentage(option.votes, totalVotes))}%)
-                  <div
-                      className={classes.poll_option_bar}
-                      style={{
-                        width: `${calculatePercentage(option.votes, totalVotes)}%`,
-                        backgroundColor: pollResultBarColors[index % 4] // Set different colors for even and odd options
-                      }}
-                    ></div>
-                  </li>
-                ))}
-              </ul>
-            ) : (
-              // Render voting options and vote button if the user hasn't voted
-              <div>
-                <ul className={classes.poll_ul}>
-                  {poll.options.map((option, index) => (
-                    <li className={classes.poll_li} key={option._id}>
-                      <label>
-                        <input
-                          type="radio"
-                          name={`poll_${poll._id}`}
-                          disabled={pollHasVoted} // Disable radio inputs if the user has voted
-                          onChange={() => handleOptionChange(index)}
-                        />
-                        {option.text}
-                      </label>
-                    </li>
-                  ))}
-                </ul>
-                <Button
-                  text="Vote"
-                  backgroundColor="steelblue"
-                  color="white"
-                  onClick={() => handleVote(poll._id, selectedPollOption)}
-                  disabled={pollHasVoted} // Disable the button if the user has voted
-                />
-              </div>
-            )}
           </div>
-        );
-      })}
-
-      <h3 className={classes.subheading_h3}>Comments</h3>
-      <form className={classes.form}>
-        <div className={classes.form_group}>
-          <label className={classes.form_label} htmlFor="name">
-            Name
-          </label>
-          <input
-            className={classes.form_input}
-            type="text"
-            placeholder="Name"
-            id="name"
-            name="name"
-            value={commentFormData.name}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div className={classes.form_group}>
-          <label className={classes.form_label} htmlFor="comment">
-            Comment
-          </label>
-          <textarea
-            placeholder="Your Comment"
-            className={classes.form_textarea}
-            rows="5"
-            id="comment"
-            name="commentText"
-            value={commentFormData.commentText}
-            onChange={handleInputChange}
-          ></textarea>
-        </div>
-        <div className={classes.addComment_div}>
-          <Button
-            text="Add Comment"
-            backgroundColor="seagreen"
-            color="white"
-            onClick={handleAddComment}
-          ></Button>
-        </div>
-      </form>
-      {episode.comments.map((comment) => {
-        return (
-          <div className={classes.comment_div} key={comment._id}>
-            <p className={classes.comment_text}>{comment.commentText}</p>
-
-            <p className={classes.comment_author}>
-              Posted by: {comment.name} on {formatDate(comment.createdAt)}
-            </p>
-            {session && (
-              <div>
-                <button
+          {modalOpen && (
+            <ModalForm
+              onClose={closeModal}
+              modalTitle={"Add Poll"}
+              modalOpen={modalOpen}
+              setModalOpen={setModalOpen}
+              form={
+                <AddPollForm
+                  onSubmit={handleAddPoll}
+                  onSubmitSuccess={closeModal}
+                />
+              }
+            />
+          )}
+          {episode.polls.map((poll) => {
+            const pollHasVoted = hasVoted[poll._id];
+            const totalVotes = poll.options.reduce(
+              (acc, option) => acc + option.votes,
+              0
+            );
+            return (
+              <div className={classes.poll_div} key={poll._id}>
+              {session && <button
+                  type="button"
                   className={classes.delete_btn}
-                  onClick={() => handleDeleteComment(episodeId, comment._id)}
+                  onClick={() => handleDeletePoll(episodeId, poll._id)}
                 >
                   x
-                </button>
+                </button>}
+                <p className={classes.poll_question}>{poll.question}</p>
+                {pollHasVoted ? (
+                  // Render results if the user has voted
+                  <ul className={classes.poll_ul}>
+                    {poll.options.map((option, index) => (
+                      <li className={classes.poll_li_results} key={option._id}>
+                        {option.text}
+                        <div
+                          className={classes.poll_option_bar}
+                          style={{
+                            width: `${calculatePercentage(
+                              option.votes,
+                              totalVotes
+                            )}%`,
+                            backgroundColor: pollResultBarColors[index % 4], // Set different colors for even and odd options
+                          }}
+                        ></div>
+                        {option.votes} votes (
+                        {Math.round(
+                          calculatePercentage(option.votes, totalVotes)
+                        )}
+                        %)
+                      </li>
+                    ))}
+                  </ul>
+                ) : (
+                  // Render voting options and vote button if the user hasn't voted
+                  <div>
+                    <ul className={classes.poll_ul}>
+                      {poll.options.map((option, index) => (
+                        <li className={classes.poll_li} key={option._id}>
+                          <label>
+                            <input
+                              type="radio"
+                              name={`poll_${poll._id}`}
+                              disabled={pollHasVoted} // Disable radio inputs if the user has voted
+                              onChange={() => handleOptionChange(index)}
+                            />
+                            {option.text}
+                          </label>
+                        </li>
+                      ))}
+                    </ul>
+                    <Button
+                      text="Vote"
+                      backgroundColor="steelblue"
+                      color="white"
+                      onClick={() => handleVote(poll._id, selectedPollOption)}
+                      disabled={pollHasVoted} // Disable the button if the user has voted
+                    />
+                  </div>
+                )}
               </div>
-            )}
-          </div>
-        );
-      })}
+            );
+          })}
+        </div>
+        <div className={classes.comments_div}>
+          <h3 className={classes.subheading_h3}>Comments</h3>
+          
+          {episode.comments.map((comment) => {
+            return (
+              <div className={classes.comment_div} key={comment._id}>
+                <p className={classes.comment_text}>{comment.commentText}</p>
+
+                <p className={classes.comment_author}>
+                  Posted by: {comment.name} on {formatDate(comment.createdAt)}
+                </p>
+                {session && (
+                  <div>
+                    <button
+                      className={classes.delete_btn}
+                      onClick={() =>
+                        handleDeleteComment(episodeId, comment._id)
+                      }
+                    >
+                      x
+                    </button>
+                  </div>
+                )}
+              </div>
+            );
+          })}
+          <form className={classes.form}>
+            <div className={classes.form_group}>
+              <label className={classes.form_label} htmlFor="name">
+                Name
+              </label>
+              <input
+                className={classes.form_input}
+                type="text"
+                placeholder="Name"
+                id="name"
+                name="name"
+                value={commentFormData.name}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className={classes.form_group}>
+              <label className={classes.form_label} htmlFor="comment">
+                Comment
+              </label>
+              <textarea
+                placeholder="Your Comment"
+                className={classes.form_textarea}
+                rows="5"
+                id="comment"
+                name="commentText"
+                value={commentFormData.commentText}
+                onChange={handleInputChange}
+              ></textarea>
+            </div>
+            <div className={classes.addComment_div}>
+              <Button
+                text="Add Comment"
+                backgroundColor="seagreen"
+                color="white"
+                onClick={handleAddComment}
+              ></Button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
