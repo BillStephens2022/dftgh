ada00a79d1427a4778d02bad3c56cb5274941e31
BillStephens2022 <two4onebill@yahoo.com>
2023-11-09 17:43:29 -0500

    removed experimental code on testimonials page

--- pages/testimonials/index.js
+++ pages/testimonials/index.js
@@ -1,6 +1,4 @@
 import { Fragment } from "react";
-import { GoComment } from "react-icons/go";
-import { RiBarChart2Fill } from 'react-icons/ri';
 import classes from "./testimonials.module.css";
 
 function Testimonials() {
@@ -8,30 +6,6 @@ function Testimonials() {
     <Fragment>
       <main className={classes.main}>
         <h1 className={classes.title}>Testimonials</h1>
-        <div className={classes.container}>
-          <div className={classes.wrapper}>
-            <div className={classes.banner_image}> </div>
-            <h1 className={classes.card_header}>NO MORE POD AND JAM</h1>
-            <p className={classes.card_p}>Aired: Oct 20, 2023</p>
-            <p className={classes.card_p}>
-              OB and Ed discuss how Ed is doing as a single parent 
-              this week including once again revisting his daughters laundry
-              habits. <br />
-             <p>The conversation is interupted by OB's outrage about how Ed does
-              his morning bagels. </p>
-              <p>After discussing food and chores the subject turns to Rutgers
-              Homecoming and the rain. </p>
-            </p>
-          </div>
-          <div className={classes.button_wrapper}>
-            <button className={`${classes.btn} ${classes.outline}`}>
-            <GoComment size={24} /> Comments
-            </button>
-            <button className={`${classes.btn} ${classes.fill}`}>
-            <RiBarChart2Fill size={24} /> Polls
-            </button>
-          </div>
-        </div>
       </main>
     </Fragment>
   );
