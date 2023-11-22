14cdcea58ac79abeb20c67caab4f57b4d3be47a4
BillStephens2022 <two4onebill@yahoo.com>
2023-11-22 08:34:26 -0500

    updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.join(__dirname, './'); // Assuming your root is where the 'pages' directory resides
+    return config;
+  },
 }
 
 module.exports = nextConfig
