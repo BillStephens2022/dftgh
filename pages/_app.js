ed92180d91deae79b17b5cae5367f0759fbcbce4
BillStephens2022 <two4onebill@yahoo.com>
2024-01-22 12:57:20 -0500

    modified meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -12,9 +12,9 @@ const App = ({
         <Layout>
           <Head>
             <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <meta property="og:title" content="Drinking From the Garden Hose - The Gen X Podcast" />
-            <meta property="og:description" content="Drinking From The Garden Hose Podcast w/Ed Philipp and OB Spencer. 2 guys talking about how it used to be...and now they are cranky old men in training...but they aren't there yet." />
-            <meta name="og:keywords" content="Podcast, Podcasts, Drinking From The Garden Hose, Generation X, Ed Philipp, OB, Obadiah Spencer, Top Podcasts, Fun Podcasts, Generation X Podcast" />
+            <meta property="og:title" content="Drinking From the Garden Hose" />
+            <meta property="og:description" content="Drinking From The Garden Hose podcast w/Ed Philipp and OB Spencer. 2 guys talking about how it used to be...and now they are cranky old men in training...but they aren't there yet." />
+            <meta name="og:keywords" content="Podcast, Podcasts, Drinking From The Garden Hose, Generation X, Ed Philipp, OB, OB Spencer, Top Podcasts, Fun Podcasts, Generation X Podcast" />
             <meta name="google-site-verification" content="qxZwsIkUNeqRkaDbKtWLky2-URdkA2OB5OWYgNk5aE0" />
           </Head>
           <Component {...pageProps} />
