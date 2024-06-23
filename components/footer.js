e04fe77b6442b1576b977a9716ccbf69d3d49339
BillStephens2022 <two4onebill@yahoo.com>
2024-06-23 06:07:17 -0400

    updated copyright year in footer to be dynamic

--- components/footer.js
+++ components/footer.js
@@ -3,10 +3,11 @@ import { GoPasskeyFill } from "react-icons/go";
 import classes from "@/components/footer.module.css";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
   return (
     <div className={classes.footer}>
       <Link href="/admin"><div className={classes.login_icon}><GoPasskeyFill fill="white" size="16px" /></div></Link>
-      <p className={classes.text}>Copyright © 2023, <Link className={classes.link} href="https://billstephens2022.github.io/my_portfolio/">Bill Stephens</Link>. All Rights Reserved.</p>
+      <p className={classes.text}>Copyright © {currentYear}, <Link className={classes.link} href="https://billstephens2022.github.io/my_portfolio/">Bill Stephens</Link>. All Rights Reserved.</p>
     </div>
   );
 }
